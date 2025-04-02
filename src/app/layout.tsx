4a3a172d36f3686eb9e9d2c2f2e380b3f5dfd6cf
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-04-02 12:10:29 -0300

    seo project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,19 @@ import type { Metadata } from "next";
 import { Nunito, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/darkmode/theme-provider";
 import { Toaster } from "sonner";
 import { setDefaultOptions } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import ClientProviders from "@/components/shared/clitent-providers";
-
 const nunito = Nunito({ subsets: ["latin"], variable: "--font-nunito" });
 const nunitoSans = Nunito_Sans({ subsets: ['latin'], variable: '--font-nunito-sans' })
 
 export const metadata: Metadata = {
   title: "Resume Creator",
   description: "Generate you Virtual Curriculum",
+  icons: {
+    icon: '/favicon.svg'
+  }
 };
 
 setDefaultOptions({locale: ptBR})
