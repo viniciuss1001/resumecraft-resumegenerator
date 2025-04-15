290ec21a75df64ad836b692f153eede13da63508
Vinicius Frota <viniciussantosfrota10012017@gmail.com>
2025-04-15 18:38:10 -0300

    profile page II

--- src/components/pages/dashboard/UserDropDown.tsx
+++ src/components/pages/dashboard/UserDropDown.tsx
@@ -10,15 +10,15 @@ import {
 import { LogOut, SquareUser } from "lucide-react"
 import Link from "next/link"
 import { User } from "next-auth"
-import {signOut} from 'next-auth/react'
+import { signOut } from 'next-auth/react'
 
 
 type UserDropDownProps = {
   user?: User
 }
 
-const UserDropdownComponent = ({user}:UserDropDownProps) => {
-  if(!user) return null
+const UserDropdownComponent = ({ user }: UserDropDownProps) => {
+  if (!user) return null
 
   const initials = user?.name?.split(" ").slice(0, 2).map((name) => name[0]).join("")
   return (
@@ -26,27 +26,27 @@ const UserDropdownComponent = ({user}:UserDropDownProps) => {
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' className="w-full gap-2 justify-start px-2">
           <Avatar>
-            <AvatarImage src={user?.image ?? ""} alt='Avatar'/>
+            <AvatarImage src={user?.image ?? ""} alt='Avatar' />
             <AvatarFallback>
               {initials}
             </AvatarFallback>
           </Avatar>
-            <p>{user?.name}</p>
+          <p>{user?.name}</p>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="center"
-      className="w-[var(--radix-dropdown-menu-trigger-width)]"
+        className="w-[var(--radix-dropdown-menu-trigger-width)]"
       >
         <Link passHref href='/dashboard/account'>
-        <DropdownMenuItem>
-          <SquareUser size={16}/>
-          Configurações de Conta
-        </DropdownMenuItem>
+          <DropdownMenuItem>
+            <SquareUser size={16} />
+            Configurações de Conta
+          </DropdownMenuItem>
         </Link>
-        <DropdownMenuItem className="text-red-500" 
-        onClick={() => signOut({callbackUrl: "/auth/login"})}
+        <DropdownMenuItem className="text-red-500"
+          onClick={() => signOut({ callbackUrl: "/auth/login" })}
         >
-          <LogOut size={16}/>
+          <LogOut size={16} />
           Sair
         </DropdownMenuItem>
       </DropdownMenuContent>
